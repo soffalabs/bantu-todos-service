2f2f891d1da87621259731992fd57723bd9b2a9c
Titus Gahissy <titus@gahissy.com>
2022-02-14 02:08:15 +0000

    Foundation update

--- bantu-todos-service/src/main/kotlin/dev/bantu/todos/gateways/config/LocalTenantsProvider.kt
+++ bantu-todos-service/src/main/kotlin/dev/bantu/todos/gateways/config/LocalTenantsProvider.kt
@@ -1,15 +1,17 @@
 package dev.bantu.todos.gateways.config
 
 import dev.bantu.accounts.api.operation.GetTenantList
+import io.soffa.foundation.api.Operation
 import io.soffa.foundation.data.TenantsLoader
 import io.soffa.foundation.messages.BinaryClient
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class LocalTenantsProvider : TenantsLoader {
 
     override fun getTenantList(client: BinaryClient): Set<String> {
-        val list = client.request("bantu-accounts", GetTenantList::class.java).get()
+        val list = client.requestWithServiceToken("bantu-accounts", GetTenantList::class.java, Operation.NO_INPUT).get(30, TimeUnit.SECONDS)
         return list.tenants.toSet()
     }
 }
