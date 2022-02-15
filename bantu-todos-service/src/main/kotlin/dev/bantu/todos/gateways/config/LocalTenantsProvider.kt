52ce96d6e709a64fba4b4d59e77d8483546182f1
Titus Gahissy <titus@gahissy.com>
2022-02-15 14:09:59 +0000

    Foundation update 1.6.3

--- bantu-todos-service/src/main/kotlin/dev/bantu/todos/gateways/config/LocalTenantsProvider.kt
+++ bantu-todos-service/src/main/kotlin/dev/bantu/todos/gateways/config/LocalTenantsProvider.kt
@@ -3,10 +3,12 @@ package dev.bantu.todos.gateways.config
 import dev.bantu.accounts.api.AccountClient
 import dev.bantu.accounts.api.operation.GetTenantList
 import io.soffa.foundation.api.Operation
+import io.soffa.foundation.commons.Logger
 import io.soffa.foundation.context.RequestContextHolder
 import io.soffa.foundation.data.TenantsLoader
 import io.soffa.foundation.messages.PubSubClient
 import org.springframework.stereotype.Component
+import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -14,13 +16,22 @@ class LocalTenantsProvider(
     private val client: PubSubClient
 ) : TenantsLoader {
 
+    companion object {
+        val logger = Logger.get(LocalTenantsProvider::class.java)!!
+    }
+
     override fun getTenantList(): Set<String> {
-        val res = client.request(
-            AccountClient.ID,
-            GetTenantList::class.java,
-            Operation.NO_INPUT,
-            RequestContextHolder.getOrCreate().withServiceToken()
-        ).get(5, TimeUnit.MINUTES)
-        return res.tenants.toSet()
+        return try {
+            val res = client.request(
+                AccountClient.ID,
+                GetTenantList::class.java,
+                Operation.NO_INPUT,
+                RequestContextHolder.getOrCreate().withServiceToken()
+            ).get(5, TimeUnit.MINUTES)
+            res.tenants.toSet()
+        }catch (e: ExecutionException){
+            logger.error("Unable to fetch tenants list from bantu-accounts, make sure the service is reachable")
+            emptySet();
+        }
     }
 }
