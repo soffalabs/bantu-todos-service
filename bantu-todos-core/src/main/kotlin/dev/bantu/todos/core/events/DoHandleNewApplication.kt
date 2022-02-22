1059d731fdbe1e97bbc48d0c47956eb36682f24d
Titus Gahissy <titus@gahissy.com>
2022-02-22 18:02:16 +0000

    Use events from Accounts

--- bantu-todos-core/src/main/kotlin/dev/bantu/todos/core/events/DoHandleNewApplication.kt
+++ bantu-todos-core/src/main/kotlin/dev/bantu/todos/core/events/DoHandleNewApplication.kt
@@ -2,28 +2,26 @@ package dev.bantu.todos.core.events
 
 import dev.bantu.accounts.api.Accounts
 import dev.bantu.accounts.api.model.Application
+import dev.bantu.accounts.api.operation.events.OnApplicationCreated
 import io.soffa.foundation.annotations.Handle
 import io.soffa.foundation.commons.Logger
-import io.soffa.foundation.core.Operation
 import io.soffa.foundation.core.RequestContext
 import io.soffa.foundation.core.db.DB
-import io.soffa.foundation.models.commons.Ack
 import javax.inject.Named
 
 @Named
 @Handle(Accounts.APPLICATION_CREATED)
-open class OnApplicationCreated(private val db: DB) : Operation<Application, Ack> {
+open class DoHandleNewApplication(private val db: DB) : OnApplicationCreated {
 
     companion object {
-        val LOG = Logger.get(OnApplicationCreated::class.java)!!
+        val LOG = Logger.get(DoHandleNewApplication::class.java)!!
     }
 
-    override fun handle(input: Application, context: RequestContext): Ack {
+    override fun handle(input: Application, context: RequestContext) {
         val tenantId = input.id!!.value
         LOG.info("A new application [%s] was created, applying migrations", tenantId)
         db.applyMigrations(tenantId)
         db.applyMigrations("${tenantId}_test")
-        return Ack.OK
     }
 
 }
